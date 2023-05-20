276db6c75b00955b2b4311da3159ae8dbce3a8b9
SikiTech <SikiTech@163.com>
2023-05-20 10:58:44 +0800

    Add JDK动态代理

---
+++ src/main/java/com/beicheng/road/proxy/SimpleJDKDynamicProxyDemo.java
@@ -0,0 +1,47 @@
+package com.beicheng.road.proxy;
+
+import javax.sound.midi.Soundbank;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * JDK动态代理
+ * @author beicheng
+ */
+public class SimpleJDKDynamicProxyDemo {
+
+    interface IService {
+        void sayHello();
+    }
+
+    static class RealService implements IService {
+        @Override
+        public void sayHello() {
+            System.out.println("hello");
+        }
+    }
+
+    static class SimpleInvocationHandler implements InvocationHandler {
+        private Object realObject;
+
+        public SimpleInvocationHandler(Object realObject) {
+            this.realObject = realObject;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("entering sayHello");
+            Object ret = method.invoke(realObject, args);
+            System.out.println("leaving sayHello");
+            return ret;
+        }
+    }
+
+    public static void main(String[] args) {
+        RealService realService = new RealService();
+        InvocationHandler handler = new SimpleInvocationHandler(realService);
+        IService proxyInstance = (IService) Proxy.newProxyInstance(realService.getClass().getClassLoader(), new Class[]{IService.class}, handler);
+        proxyInstance.sayHello();
+    }
+}
